e31403a0e42e2019eaa4c3be10e431d231318435
niyoh <zhrh120@gmail.com>
2019-12-21 22:36:12 +0800

    update version

--- Formula/baidupcs-go.rb
+++ Formula/baidupcs-go.rb
@@ -1,16 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class BaidupcsGo < Formula
   desc "百度网盘客户端 - Go语言编写"
   homepage "https://github.com/iikira/BaiduPCS-Go"
-  url "https://github.com/iikira/BaiduPCS-Go/releases/download/v3.5.3/BaiduPCS-Go-v3.5.3-darwin-osx-amd64.zip"
-  version "3.5.3"
-  sha256 "f71c207070e78222daaff6be1be24531ade1eed761e5ac8a5334f65da60e7ec5"
-  # depends_on "cmake" => :build
+  url "https://github.com/iikira/BaiduPCS-Go/archive/v3.6.1.tar.gz"
+  sha256 "07e1d802e6b49425a53b883c354d8b753de55f7005ac8c7b27cd8396db0447da"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "baidupcs-go"
+    system "go", "build", "-o", bin/"baidupcs-go"
   end
 
+  test do
+    system bin/"baidupcs-go", "--version"
+  end
 end
